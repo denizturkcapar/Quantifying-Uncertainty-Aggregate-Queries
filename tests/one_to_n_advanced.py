f27ded1522b7d9321741fb1fe7c5109156035167
Deniz Turkcapar <dturkcapar@gmail.com>
2020-05-10 14:53:48 -0500

    file cleanup

--- tests/one_to_n_advanced.py
+++ tests/one_to_n_advanced.py
@@ -2,7 +2,6 @@ import sys
 from os import path
 sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
 from src import one_to_n
-#from src import helpers
 
 import datetime
 import textdistance
@@ -13,10 +12,6 @@ import networkx as nx
 table_a = one_to_n.lat_convert_df("ACM.csv")
 
 table_b = one_to_n.lat_convert_df("DBLP2.csv")
-
-
-#repeat = one_to_n.create_duplicates(table_a, "id", 3)
-#print("The duplicated table is as follows:", repeat, "\n")
 
 now = datetime.datetime.now()
 treshold_graph_maximal = one_to_n.valcomp_treshold_updated_maximal_construct_graph(table_a, table_b, 0.5)
@@ -49,4 +44,4 @@ print("The de-duplicated final result is:", last, "\n")
 
 
 check = one_to_n.more_than_one(last)
-print("The non 1:n matchings found in this dataset are:", check, "\n")
+print("The 1:n matchings found in this dataset are:", check, "\n")
